c934ae6dfbc6ada096b0f00ad63ce16755d0eb55
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-10-06 19:44:13 -0300

    pronto?

--- script.js
+++ script.js
@@ -29,9 +29,13 @@ function calculateAbsences() {
         const resultImage = document.getElementById('result-image');
 
         if (remainingAbsences >= 0) {
+            document.getElementById('result-message').classList.remove('hidden');
+            document.getElementById('result-image').classList.remove('hidden');
             resultMessage.textContent = `Você ainda pode faltar ${remainingAbsences} horas.`;
             resultImage.src = 'images/falte.jpeg'; 
         } else {
+            document.getElementById('result-message').classList.remove('hidden');
+            document.getElementById('result-image').classList.remove('hidden');
             resultMessage.textContent = `Você ultrapassou o limite de faltas em ${Math.abs(remainingAbsences)} horas.`;
             resultImage.src = 'images/reprovou.gif'; 
         }
@@ -41,14 +45,19 @@ function calculateAbsences() {
     }
 }
 
-function goBack() {
+function goBackToInitial() {
     document.getElementById('absence-screen').classList.add('hidden');
     document.getElementById('result-screen').classList.add('hidden');
+    document.getElementById('result-message').classList.add('hidden');
+    document.getElementById('result-image').classList.add('hidden');
     document.getElementById('initial-screen').classList.remove('hidden');
+    document.getElementById('custom-hours-input').classList.add('hidden');
+    document.getElementById('absence-hours').value = '';
+    totalHours = 0;
 }
 
 document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     const themeIcon = document.getElementById('theme-icon');
-    themeIcon.src = document.body.classList.contains('dark-mode') ? 'images/escuro.png' : 'images/claro.png';
+    themeIcon.src = document.body.classList.contains('dark-mode') ? 'images/claro.png' : 'images/claro.png';
 });
