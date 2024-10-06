63a281b07ca9cbd79a8529e3f2d4b76de58b98c1
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-10-06 19:57:05 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let totalHours = 0;
 
 function selectHours(hours) {
+    if (hours < 0) {
+        alert("A quantidade de horas não pode ser negativa.");
+        return;
+    }
     totalHours = hours;
     document.getElementById('initial-screen').classList.add('hidden');
     document.getElementById('absence-screen').classList.remove('hidden');
@@ -12,6 +16,10 @@ function showCustomHoursInput() {
 
 function selectCustomHours() {
     const customHours = document.getElementById('custom-hours').value;
+    if (customHours < 0) {
+        alert("A quantidade de horas não pode ser negativa.");
+        return;
+    }
     if (customHours) {
         totalHours = parseInt(customHours);
         document.getElementById('custom-hours-input').classList.add('hidden');
@@ -22,6 +30,10 @@ function selectCustomHours() {
 
 function calculateAbsences() {
     const absenceHours = document.getElementById('absence-hours').value;
+    if (absenceHours < 0) {
+        alert("A quantidade de faltas não pode ser negativa.");
+        return;
+    }
     if (absenceHours) {
         const allowedAbsences = Math.floor(totalHours * 0.25);
         const remainingAbsences = allowedAbsences - Math.floor(absenceHours);
@@ -29,13 +41,9 @@ function calculateAbsences() {
         const resultImage = document.getElementById('result-image');
 
         if (remainingAbsences >= 0) {
-            document.getElementById('result-message').classList.remove('hidden');
-            document.getElementById('result-image').classList.remove('hidden');
             resultMessage.textContent = `Você ainda pode faltar ${remainingAbsences} horas.`;
             resultImage.src = 'images/falte.jpeg'; 
         } else {
-            document.getElementById('result-message').classList.remove('hidden');
-            document.getElementById('result-image').classList.remove('hidden');
             resultMessage.textContent = `Você ultrapassou o limite de faltas em ${Math.abs(remainingAbsences)} horas.`;
             resultImage.src = 'images/reprovou.gif'; 
         }
@@ -59,5 +67,5 @@ function goBackToInitial() {
 document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     const themeIcon = document.getElementById('theme-icon');
-    themeIcon.src = document.body.classList.contains('dark-mode') ? 'images/claro.png' : 'images/claro.png';
+    themeIcon.src = document.body.classList.contains('dark-mode') ? 'images/escuro.png' : 'images/claro.png';
 });
